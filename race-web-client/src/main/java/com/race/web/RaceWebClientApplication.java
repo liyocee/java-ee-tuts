4d61a5d62c69387a4f3312f1718f6ff464488ce1
Liyosi Collins <collinskivale@gmail.com>
2015-11-16 23:22:40 +0300

    Registration of the web client

--- race-web-client/src/main/java/com/race/web/RaceWebClientApplication.java
+++ race-web-client/src/main/java/com/race/web/RaceWebClientApplication.java
@@ -2,8 +2,10 @@ package com.race.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RaceWebClientApplication {
 
     public static void main(String[] args) {
